e7cffd9dcef5abafe147a49fad14e3c315268091
ShinobuTakahashi <a.little.more.than.before@gmail.com>
2025-05-13 11:55:57 +0900

    Fix eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,35 @@
-const eslintConfig = require("@akashic/eslint-config");
 const eslintConfigPrettier = require("eslint-config-prettier");
 const storybook = require("eslint-plugin-storybook");
 const unuserdPlugin = require("eslint-plugin-unused-imports");
 const vueParser = require("vue-eslint-parser");
+const importPlugin = require("eslint-plugin-import");
+const globals = require("globals");
 
 module.exports = [
-  ...eslintConfig,
+  importPlugin.flatConfigs.recommended,
   eslintConfigPrettier,
   ...storybook.configs["flat/recommended"],
   {
-    files: ["src/**/*.{vue}"],
+    files: ["src/**/*.{ts,vue}"],
     languageOptions: {
       parser: vueParser,
       sourceType: "module",
+      ecmaVersion: 2020,
       parserOptions: {
-        ecmaVersion: 2020,
+        parser: "@typescript-eslint/parser",
         ecmaFeatures: {
           jsx: true
         }
-      }
+      },
+      globals: {
+        ...globals.node,
+        AE: true,
+      },
     },
+    plugins: { "unused-imports": unuserdPlugin },
+    ignores: [
+      "public/**/*.d.ts"
+    ],
     rules: {
       "vue/html-indent": "off",
       "vue/attribute-hyphenation": "off",
@@ -33,34 +43,8 @@ module.exports = [
       "unused-imports/no-unused-imports": "error",
       "unused-imports/no-unused-vars": [
         "off" // TODO: 誤検知のため一旦 off
-      ]
-    },
-  },
-  {
-      files: ["src/**/*.ts"],
-      languageOptions: {
-        sourceType: "module",
-        parserOptions: {
-          project: "tsconfig.json",
-          ecmaVersion: 2020,
-        }
-      },
-      ignores: [
-        "public/**/*.d.ts"
       ],
-      plugins: { "unused-imports": unuserdPlugin },
-      rules: {
-        "import/order": ["error", {
-          "alphabetize": {
-            "order": "asc",
-            "caseInsensitive": true
-          }
-        }],
-        "@typescript-eslint/no-unused-vars": "off",
-        "unused-imports/no-unused-imports": "error",
-        "unused-imports/no-unused-vars": [
-          "off" // TODO: 誤検知のため一旦 off
-        ]
-      }
+      "import/no-unresolved": "off"
+    }
   }
 ];
